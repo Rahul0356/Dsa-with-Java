937c3cba1c71206165c3c5a153942ea3d6ada8ce
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-05-16 14:54:33 +0530

    AVg of 3 numbers

--- addNum.java
+++
@@ -1,9 +0,0 @@
-public class addNum{
-    public static void main(String args[]){
-        int a=2;
-        int b=5;
-        int sum =a+b;
-        System.out.println("Sum of a and b is:"+sum);
-    }
-
-}
