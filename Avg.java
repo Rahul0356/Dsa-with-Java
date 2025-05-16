07ac4825c7a71a62f6a89a5aa06b71f7cfe08f78
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-05-16 14:54:55 +0530

    avg of 3 numbers

---
+++ Avg.java
@@ -0,0 +1,10 @@
+public class Avg{
+    public static void main(String[] args){
+
+        int a=2;
+        int b=3;
+        int c=4;
+        int avg= (a+b+c)/3;
+        System.out.println("Average is: " + avg);
+    }
+}
