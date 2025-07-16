5e1538f8c0d4310912a958e42d2da3c7ac158301
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-07-16 18:25:52 +0530

    basic code

---
+++ loop/basicloop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class basicloop {
+
+    public static void main(String args[]){
+        System.out.println("Hello, World!");
+        System.out.println("This is a basic loop example.");
+        System.out.println("Let's print numbers from 1 to 5:");
+
+    }
+}
+
+
+// so you can use loop
+
+// for
+//while
+//do while
---
+++ loop/helloloop.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class helloloop{
+
+    public static void main(String args[]){
+        int counter =0;
+        while(counter<100){
+            System.out.println("Hello world");
+            counter++;
+
+        }
+        System.out.println("priner HW 100x");
+    }
+}
