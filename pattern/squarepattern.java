04edf9172dbc7c6cb604b5b690bd3ea062f42067
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-07-18 09:26:35 +0530

    squarepattern

---
+++ pattern/squarepattern.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class squarepattern{
+    public static void main(String arg[]){
+        for(int i=1;i<=4;i++){
+            System.out.println("****");
+        }
+    }
+}
